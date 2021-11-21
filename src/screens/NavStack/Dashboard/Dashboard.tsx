c07096c2a7bf514afdc2cfeba3b7d05d7422b0de
Coodos <info@coodos.co>
2021-11-21 11:15:54 +0530

    partial-cred-reaveal

--- src/screens/NavStack/Dashboard/Dashboard.tsx
+++ src/screens/NavStack/Dashboard/Dashboard.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { VerifiableCreds } from "../../../data/vc.sample";
 import { Card } from "../../../components/ui/Card/Card";
 import { ChevronRight } from "../../../components/assets/icons";
 import { Container } from "../../../components/ui/Container/Container";
 import "./Dashboard.scss";
+import { VerifiableCredential } from "../VerifiableCredential/VerifiableCredential";
 
 export const Dashboard = () => {
+  const [selectedVC, setSelectedVC] = useState<Record<string, unknown>>();
+
   return (
     <Container>
-      {VerifiableCreds.map((vc, index) => (
-        <Card key={index}>
-          <div className="verifiable-cred" style={{ cursor: "pointer" }}>
-            <div className="vc-icon">
-              {vc.credentialSubject.credIssuer[0].toUpperCase()}
+      {selectedVC && (
+        <VerifiableCredential vc={selectedVC} setSelected={setSelectedVC} />
+      )}
+      {!selectedVC &&
+        VerifiableCreds.map((vc, index) => (
+          <Card key={index}>
+            <div
+              className="verifiable-cred"
+              onClick={() => setSelectedVC(() => vc)}
+              style={{ cursor: "pointer" }}
+            >
+              <div className="vc-icon">
+                {vc.credentialSubject.credIssuer[0].toUpperCase()}
+              </div>
+              <div className="vc-data">
+                <div className="vc-issuer">
+                  {vc.credentialSubject.credIssuer}
+                </div>
+                <div className="vc-type">{vc.credentialSubject.credType}</div>
+              </div>
+              <ChevronRight color={"white"} />
             </div>
-            <div className="vc-data">
-              <div className="vc-issuer">{vc.credentialSubject.credIssuer}</div>
-              <div className="vc-type">{vc.credentialSubject.credType}</div>
-            </div>
-            <ChevronRight color={"white"} />
-          </div>
-        </Card>
-      ))}
+          </Card>
+        ))}
     </Container>
   );
 };
