eff9e7000403cd0504f70590fdf8b63d5ce8937e
Coodos <info@coodos.co>
2021-11-22 13:52:10 +0530

    added link to settings

--- src/components/ui/TopBar/TopBar.tsx
+++ src/components/ui/TopBar/TopBar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NotifBell } from "../../assets/icons";
 import { IUser } from "../../../types/user.interface";
 import { ViraLogoWhite } from "../../assets/ViraLogoWhite";
+import { useNavigate } from "react-router";
 import "./TopBar.scss";
 
 interface ITopBarProps {
@@ -9,9 +10,13 @@ interface ITopBarProps {
 }
 export const TopBar = (props: ITopBarProps) => {
   const { user } = props;
+  const navigate = useNavigate();
+
   return (
     <div className="topbar">
-      <div className="profile">{user.profile[0].toUpperCase()}</div>
+      <div className="profile" onClick={() => navigate("/settings")}>
+        {user.profile[0].toUpperCase()}
+      </div>
       <ViraLogoWhite height={40} width={40} />
       <NotifBell heigh={40} width={40} />
     </div>
